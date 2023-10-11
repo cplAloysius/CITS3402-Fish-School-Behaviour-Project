077e1cb2c63e01ff1d3d6d00e70d246f35c6def4
cplAloysius <99251110+cplAloysius@users.noreply.github.com>
2023-10-11 13:49:37 +0800

    not copied

--- sequential_MPI.c
+++ sequential_MPI.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <mpi.h>
 
 #define NUM_FISH 1000000 // Number of fish in the school
 #define W_INITIAL 10.0   // Initial weight of each fish
-
+#define MASTER 0
 
 // Structure to represent a fish
 typedef struct
@@ -14,31 +15,69 @@ typedef struct
     double weight;
 } Fish;
 
-// Function to calculate the objective function for a fish
-double calculateObjective(Fish *fish)
-{
-    return sqrt(fish->x * fish->x + fish->y * fish->y);
+void write_to_file(double x, double y, double weight, int no_fish, const char *filename) {
+    FILE *file = fopen(filename, "w");
+    for (int i = 0; i < no_fish; i++) {
+        fprintf(file, "%.2lf %.2lf %.2lf\n", x, y, weight);
+    }
+    fclose(file);
 }
 
 int main()
 {
+    FILE *fp1, *fp2, *fopen();
 
-    // Create an array of fish dynamically
-    Fish *school = (Fish *)malloc(NUM_FISH * sizeof(Fish));
-    if (school == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return 1;
+    int node_id, no_nodes;
+    MPI_Init(0, 0);
+    MPI_Comm_rank(MPI_COMM_WORLD, &node_id);
+	MPI_Comm_size(MPI_COMM_WORLD, &no_nodes);
+
+    Fish* school_send = NULL;
+
+    if (node_id == MASTER) {
+        school_send = (Fish *)malloc(NUM_FISH * sizeof(Fish));
+        write_to_file(school, NUM_FISH, "file1");
+	}
+    
+    int fish_nodes = NUM_FISH / no_nodes;
+    Fish *school_rec = (Fish *)malloc(fish_nodes * sizeof(Fish));
+
+    MPI_Scatter(school_send, fish_nodes, MPI_DOUBLE, school_rec, fish_nodes, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+
+    Fish *school_master = NULL;
+    if (node_id == MASTER) {
+        school_master = (Fish *)malloc(NUM_FISH * sizeof(Fish));
     }
 
-    for (int i = 0; i < NUM_FISH; i++)
-    {
-        school[i].x = (double)(rand() % 201 - 100); // Random x-coordinate between -100 and 100
-        school[i].y = (double)(rand() % 201 - 100); // Random y-coordinate between -100 and 100
-        school[i].weight = W_INITIAL;
-    }
-    // Free dynamically allocated memory
-    free(school);
+    MPI_Gather(school_rec, fish_nodes, MPI_DOUBLE, school_master, fish_nodes, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
 
+    if (node_id == MASTER) {
+        write_to_file(school_master, NUM_FISH, "file2");
+    }
+
+    if (node_id == MASTER) {
+        free(school_send);
+        free(school_master);
+    }
+    free(school_rec);
+
+    MPI_Finalize();
     return 0;
+
+
+
+    // for (int i = 0; i < NUM_FISH; i++)
+    // {
+    //     school[i].x = (double)(rand() % 201 - 100); // Random x-coordinate between -100 and 100
+    //     school[i].y = (double)(rand() % 201 - 100); // Random y-coordinate between -100 and 100
+    //     school[i].weight = W_INITIAL;
+    // }
+
+    // MPI_Init(&argc, &argv);
+    // MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    // MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
+    // // Free dynamically allocated memory
+    // free(school);
+
+    // return 0;
 }
