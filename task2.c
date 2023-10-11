5ab8091027945a29fe054e65f2818ea6e11c2ae8
cplAloysius <99251110+cplAloysius@users.noreply.github.com>
2023-10-11 21:36:28 +0800

    done

--- task2.c
+++ task2.c
@@ -9,7 +9,7 @@
 #define NUM_FISH 100000
 #define NUM_STEPS 300 
 #define MASTER 0
-#define NUM_FIELDS 4
+#define FIELDS 4
 #define W_INITIAL 10
 #define W_MAX (2 * W_INITIAL)
 
@@ -22,12 +22,12 @@ typedef struct {
 } Fish;
 
 MPI_Datatype create_mpi_struct() {
-    MPI_Datatype MPI_FISH;
-    MPI_Datatype types[NUM_FIELDS];
-    int blocklengths[NUM_FIELDS];
-    MPI_Aint offsets[NUM_FIELDS];
+    MPI_Datatype MPI_SCHOOL_STRUCT;
+    MPI_Datatype types[FIELDS];
+    int blocklengths[FIELDS];
+    MPI_Aint offsets[FIELDS];
 
-    for (int i = 0; i < NUM_FIELDS; i++) {
+    for (int i = 0; i < FIELDS; i++) {
         types[i] = MPI_DOUBLE;
         blocklengths[i] = 1;
     }
@@ -37,10 +37,10 @@ MPI_Datatype create_mpi_struct() {
     offsets[2] = offsetof(Fish, delta_fi);
     offsets[3] = offsetof(Fish, weight);
 
-    MPI_Type_create_struct(NUM_FIELDS, blocklengths, offsets, types, &MPI_FISH);
-    MPI_Type_commit(&MPI_FISH);
+    MPI_Type_create_struct(FIELDS, blocklengths, offsets, types, &MPI_SCHOOL_STRUCT);
+    MPI_Type_commit(&MPI_SCHOOL_STRUCT);
 
-    return MPI_FISH;
+    return MPI_SCHOOL_STRUCT;
 }
 
 double calculateObjective(Fish *fish)
@@ -49,19 +49,19 @@ double calculateObjective(Fish *fish)
 }
 
 int main() {
-    int process_id, num_processes;
+    int node_id, num_nodes;
     MPI_Init(NULL, NULL);
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &node_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_nodes);
 
-    Fish *send_buf = NULL;
-    Fish *recv_buf = NULL;
-    const MPI_Datatype MPI_FISH = create_mpi_struct();
+    Fish *school_send = NULL;
+    Fish *school_rec = NULL;
+    const MPI_Datatype MPI_SCHOOL_STRUCT = create_mpi_struct();
 
-    if (process_id == MASTER) {
-        send_buf = (Fish *)malloc(NUM_FISH * sizeof(Fish));
-        if (send_buf == NULL) {
+    if (node_id == MASTER) {
+        school_send = (Fish *)malloc(NUM_FISH * sizeof(Fish));
+        if (school_send == NULL) {
             perror("Memory allocation error");
             MPI_Finalize();
             return 1;
@@ -69,93 +69,93 @@ int main() {
 
         for (int i = 0; i < NUM_FISH; i++) {
             // Initialize send_buf with fish data (not shown in the C code)
-            send_buf[i].x = (double)(rand() % 201 - 100); // Random x-coordinate between -100 and 100
-            send_buf[i].y = (double)(rand() % 201 - 100); // Random y-coordinate between -100 and 100
-            send_buf[i].weight = W_INITIAL;
+            school_send[i].x = (double)(rand() % 201 - 100); // Random x-coordinate between -100 and 100
+            school_send[i].y = (double)(rand() % 201 - 100); // Random y-coordinate between -100 and 100
+            school_send[i].weight = W_INITIAL;
         }
     }
 
-    const int num_fish_per_process = NUM_FISH / num_processes;
-    recv_buf = (Fish *)malloc(num_fish_per_process * sizeof(Fish));
+    const int fish_per_node = NUM_FISH / num_nodes;
+    school_rec = (Fish *)malloc(fish_per_node * sizeof(Fish));
 
-    MPI_Scatter(send_buf, num_fish_per_process, MPI_FISH, recv_buf, num_fish_per_process, MPI_FISH, MASTER, MPI_COMM_WORLD);
+    MPI_Scatter(school_send, fish_per_node, MPI_SCHOOL_STRUCT, school_rec, fish_per_node, MPI_SCHOOL_STRUCT, MASTER, MPI_COMM_WORLD);
 
     double start_time = omp_get_wtime();
 
     for (int steps = 0; steps < NUM_STEPS; steps++) {
-        double max_difference = 0.0;
-        double local_numerator = 0.0;
-        double local_denominator = 0.0;
+        double delta_fi_max = 0.0;
+        double bari_num = 0.0;
+        double bari_denom = 0.0;
 
         #pragma omp parallel
         {
             #pragma omp for
-            for (int i = 0; i < num_fish_per_process; i++)
+            for (int i = 0; i < fish_per_node; i++)
             {
                 // Calculate change in objective function
-                double old_obj = calculateObjective(&recv_buf[i]);
-                double new_x = recv_buf[i].x + (double)(rand() % 21 - 10) / 100.0;
-                double new_y = recv_buf[i].y + (double)(rand() % 21 - 10) / 100.0;
-                recv_buf[i].x = new_x;
-                recv_buf[i].y = new_y;
-                double new_obj = calculateObjective(&recv_buf[i]);
+                double old_obj = calculateObjective(&school_rec[i]);
+                double new_x = school_rec[i].x + (double)(rand() % 21 - 10) / 100.0;
+                double new_y = school_rec[i].y + (double)(rand() % 21 - 10) / 100.0;
+                school_rec[i].x = new_x;
+                school_rec[i].y = new_y;
+                double new_obj = calculateObjective(&school_rec[i]);
                 double delta_fi = new_obj - old_obj;
-                recv_buf[i].delta_fi = delta_fi;
+                school_rec[i].delta_fi = delta_fi;
             }
 
-            #pragma omp for reduction(max : max_difference)
-            for (int i = 0; i < num_fish_per_process; i++)
+            #pragma omp for reduction(max : delta_fi_max)
+            for (int i = 0; i < fish_per_node; i++)
             {
-                if (recv_buf[i].delta_fi > max_difference)
+                if (school_rec[i].delta_fi > delta_fi_max)
                 {
-                    max_difference = recv_buf[i].delta_fi;
+                    delta_fi_max = school_rec[i].delta_fi;
                 }
             }
         }
 
-        double global_max_difference;
-        MPI_Reduce(&max_difference, &global_max_difference, 1, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
-        MPI_Bcast(&global_max_difference, 1, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+        double global_delta_fi_max;
+        MPI_Reduce(&delta_fi_max, &global_delta_fi_max, 1, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
+        MPI_Bcast(&global_delta_fi_max, 1, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
 
         #pragma omp parallel
         {
-            #pragma omp for reduction(+ : local_numerator, local_denominator)
-            for (int i = 0; i < num_fish_per_process; i++)
+            #pragma omp for reduction(+ : bari_num, bari_denom)
+            for (int i = 0; i < fish_per_node; i++)
             {
                 // Update fish weight
-                recv_buf[i].weight = fmin(W_MAX, recv_buf[i].weight + recv_buf[i].delta_fi / global_max_difference);
-                recv_buf[i].weight = fmax(0, recv_buf[i].weight);
+                school_rec[i].weight = fmin(W_MAX, school_rec[i].weight + school_rec[i].delta_fi / global_delta_fi_max);
+                school_rec[i].weight = fmax(0, school_rec[i].weight);
 
-                local_numerator += calculateObjective(&recv_buf[i]) * recv_buf[i].weight;
-                local_denominator += calculateObjective(&recv_buf[i]);
+                bari_num += calculateObjective(&school_rec[i]) * school_rec[i].weight;
+                bari_denom += calculateObjective(&school_rec[i]);
             }
         }
 
-        double global_numerator;
-        double global_denominator;
-        MPI_Reduce(&local_numerator, &global_numerator, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
-        MPI_Reduce(&local_denominator, &global_denominator, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+        double global_num;
+        double global_denom;
+        MPI_Reduce(&bari_num, &global_num, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+        MPI_Reduce(&bari_denom, &global_denom, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
 
-        if (process_id == MASTER) {
-            double barycentre = global_numerator / global_denominator;
+        if (node_id == MASTER) {
+            double barycentre = global_num / global_denom;
             if (barycentre <= 0) {
                 fprintf(stderr, "Exception: barycentre is less than or equal to 0\n");
                 MPI_Finalize();
                 return 1;
             }
 
-            printf("%lf\n", barycentre);
+            printf("Step %d - Barycenter: %lf\n", steps + 1, barycentre);
         }
     }
 
-    if (process_id == MASTER) {
+    if (node_id == MASTER) {
         double end_time = omp_get_wtime();
         double elapsed_time = end_time - start_time;
         printf("Elapsed Time: %lf seconds\n", elapsed_time);
     }
 
-    free(send_buf);
-    free(recv_buf);
+    free(school_send);
+    free(school_rec);
     MPI_Finalize();
     return 0;
 }
